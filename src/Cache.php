3ff453f3e72b4c8bbb5939593c6216e082d05342
Kasper Hansen <kasper@bookstudenterkorsel.dk>
2023-08-28 14:32:47 +0200

    disabled both apcu and memcached

--- src/Cache.php
+++ src/Cache.php
@@ -18,21 +18,21 @@ class Cache implements iCache
      */
     public function store(string $key, float|object|int|bool|array|string $value, int $ttl = 600): array|bool|float|int|object|string
     {
-        if (extension_loaded(extension: 'memcached')) {
-            $memcached = new Memcached();
+//        if (extension_loaded(extension: 'memcached')) {
+//            $memcached = new Memcached();
+//
+//            if (isset($_ENV['MEMCACHED_HOST'], $_ENV['MEMCACHED_PORT'])) {
+//                if ($memcached->addServer(host: $_ENV['MEMCACHED_HOST'], port: (int)$_ENV['MEMCACHED_PORT'])) {
+//                    $memcached->set(key: $key, value: $value, expiration: time() + $ttl);
+//
+//                    return $value;
+//                }
+//            }
+//        }
 
-            if (isset($_ENV['MEMCACHED_HOST'], $_ENV['MEMCACHED_PORT'])) {
-                if ($memcached->addServer(host: $_ENV['MEMCACHED_HOST'], port: (int)$_ENV['MEMCACHED_PORT'])) {
-                    $memcached->set(key: $key, value: $value, expiration: time() + $ttl);
-
-                    return $value;
-                }
-            }
-        }
-
-        if (extension_loaded(extension: 'apcu')) {
-            apcu_store($key, $value, $ttl);
-        }
+//        if (extension_loaded(extension: 'apcu')) {
+//            apcu_store($key, $value, $ttl);
+//        }
 
         return $value;
     }
@@ -42,23 +42,23 @@ class Cache implements iCache
      */
     public function fetch(string $key): array|bool|float|int|object|null|string
     {
-        if (extension_loaded(extension: 'memcached')) {
-            $memcached = new Memcached();
+//        if (extension_loaded(extension: 'memcached')) {
+//            $memcached = new Memcached();
+//
+//            if (isset($_ENV['MEMCACHED_HOST'], $_ENV['MEMCACHED_PORT'])) {
+//                if ($memcached->addServer(host: $_ENV['MEMCACHED_HOST'], port: (int)$_ENV['MEMCACHED_PORT'])) {
+//                    if ($value = $memcached->get(key: $key)) {
+//                        return $value;
+//                    }
+//                }
+//            }
+//        }
 
-            if (isset($_ENV['MEMCACHED_HOST'], $_ENV['MEMCACHED_PORT'])) {
-                if ($memcached->addServer(host: $_ENV['MEMCACHED_HOST'], port: (int)$_ENV['MEMCACHED_PORT'])) {
-                    if ($value = $memcached->get(key: $key)) {
-                        return $value;
-                    }
-                }
-            }
-        }
-
-        if (extension_loaded(extension: 'apcu')) {
-            if ($value = apcu_fetch($key)) {
-                return $value;
-            }
-        }
+//        if (extension_loaded(extension: 'apcu')) {
+//            if ($value = apcu_fetch($key)) {
+//                return $value;
+//            }
+//        }
 
         return null;
     }
